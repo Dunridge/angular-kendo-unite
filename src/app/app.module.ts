4679c598d658e1ee0e73a8476a74ef523f4212af
MaksymPolinka <maksym.polinka@ava.codes>
2021-09-12 19:12:29 +0300

    covered l1-buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonControlPanelComponent } from './components/button-control-panel/button-control-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonControlPanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
